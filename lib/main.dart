69c11a3fc2ac0fd82bc9f02f7f5cdd3d47655889
Gen-zprogrammer <bibitraikhanazzaki@gmail.com>
2024-07-18 16:58:15 +0700

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  List<Color> color = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.amber,
+  ];
+
+  final List<Widget> list = List.generate(
+    40,
+    (index) => Text(
+      index.toString(),
+      style: const TextStyle(
+        fontSize: 20.0,
+      ),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -13,35 +28,67 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.lightBlue,
-          appBar: AppBar(
-            title: const Text('Flutter Basic'),
-            backgroundColor: Colors.greenAccent,
-          ),
-          body: Stack(
-            children: [
-              Container(
-                width: 400,
-                height: 50,
-                color: Colors.green,
-              ),
-              Container(
-                width: 300,
-                height: 50,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 200,
-                height: 50,
-                color: Colors.red,
-              ),
-              Container(
-                width: 200,
-                height: 50,
-                color: Colors.amber,
-              ),
-            ],
-          )),
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: const Text('List View'),
+          backgroundColor: Colors.greenAccent,
+        ),
+
+        body: ListView(
+          children: list,
+        ),
+        // body: ListView.separated(
+        //   itemBuilder: (context, index) {
+        //     return Container(
+        //       height: 100,
+        //       width: 100,
+        //       decoration: const BoxDecoration(
+        //         color: Colors.blue,
+        //       ),
+        //     );
+        //   },
+        //   separatorBuilder: (context, index) {
+        //     // buat memberi jarak
+        //     return const Divider(
+        //       color: Colors.red,
+        //     );
+        //   },
+        //   itemCount: color.length,
+        // ),
+        // // body: ListView.builder(
+        //   itemCount: 4,
+        //   itemBuilder: (context, index) {
+        //     return Container(
+        //       height: 300,
+        //       width: 300,
+        //       color: color[index],
+        //     );
+        //   },
+        // ),
+        // body: ListView(
+        //   // scrollDirection: Axis.horizontal,
+        //   children: [
+        //     Container(
+        //       width: 400,
+        //       height: 400,
+        //       color: Colors.green,
+        //     ),
+        //     Container(
+        //       width: 300,
+        //       height: 300,
+        //       color: Colors.blue,
+        //     ),
+        //     Container(
+        //       width: 200,
+        //       height: 200,
+        //       color: Colors.red,
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.amber,
+        //     ),
+      ),
     );
   }
 }
