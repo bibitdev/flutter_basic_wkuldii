90672687bf2fb312848ef6bf0e0a844e46ae6f39
Gen-zprogrammer <bibitraikhanazzaki@gmail.com>
2024-07-19 06:35:47 +0700

    List Tile

--- lib/main.dart
+++ lib/main.dart
@@ -33,62 +33,147 @@ class MyApp extends StatelessWidget {
           title: const Text('List View'),
           backgroundColor: Colors.greenAccent,
         ),
-
         body: ListView(
-          children: list,
+          children: const [
+            Padding(padding: EdgeInsets.all(8)),
+            ListTile(
+              title: Text('List Tile'),
+              subtitle: Text('Ini adalah subtitle'),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              trailing: Text('10:00PM'),
+              tileColor: Colors.amber,
+              // onTap: () {
+              //   return;
+              // },
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text('List Tile'),
+              subtitle: Text('Ini adalah subtitle'),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              trailing: Text('10:00PM'),
+              tileColor: Colors.amber,
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text('List Tile'),
+              subtitle: Text('Ini adalah subtitle'),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              trailing: Text('10:00PM'),
+              tileColor: Colors.amber,
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text('List Tile'),
+              subtitle: Text('Ini adalah subtitle'),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              trailing: Text('10:00PM'),
+              tileColor: Colors.amber,
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              title: Text('List Tile'),
+              subtitle: Text('Ini adalah subtitle'),
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              trailing: Text('10:00PM'),
+              tileColor: Colors.amber,
+            ),
+          ],
         ),
-        // body: ListView.separated(
-        //   itemBuilder: (context, index) {
-        //     return Container(
-        //       height: 100,
-        //       width: 100,
-        //       decoration: const BoxDecoration(
-        //         color: Colors.blue,
-        //       ),
-        //     );
-        //   },
-        //   separatorBuilder: (context, index) {
-        //     // buat memberi jarak
-        //     return const Divider(
-        //       color: Colors.red,
-        //     );
-        //   },
-        //   itemCount: color.length,
-        // ),
-        // // body: ListView.builder(
-        //   itemCount: 4,
-        //   itemBuilder: (context, index) {
-        //     return Container(
-        //       height: 300,
-        //       width: 300,
-        //       color: color[index],
-        //     );
-        //   },
-        // ),
-        // body: ListView(
-        //   // scrollDirection: Axis.horizontal,
-        //   children: [
-        //     Container(
-        //       width: 400,
-        //       height: 400,
-        //       color: Colors.green,
-        //     ),
-        //     Container(
-        //       width: 300,
-        //       height: 300,
-        //       color: Colors.blue,
-        //     ),
-        //     Container(
-        //       width: 200,
-        //       height: 200,
-        //       color: Colors.red,
-        //     ),
-        //     Container(
-        //       width: 100,
-        //       height: 100,
-        //       color: Colors.amber,
-        //     ),
       ),
+
+      // body: ListView(
+      //   children: list,
+      // ),
+      // body: ListView.separated(
+      //   itemBuilder: (context, index) {
+      //     return Container(
+      //       height: 100,
+      //       width: 100,
+      //       decoration: const BoxDecoration(
+      //         color: Colors.blue,
+      //       ),
+      //     );
+      //   },
+      //   separatorBuilder: (context, index) {
+      //     // buat memberi jarak
+      //     return const Divider(
+      //       color: Colors.red,
+      //     );
+      //   },
+      //   itemCount: color.length,
+      // ),
+      // // body: ListView.builder(
+      //   itemCount: 4,
+      //   itemBuilder: (context, index) {
+      //     return Container(
+      //       height: 300,
+      //       width: 300,
+      //       color: color[index],
+      //     );
+      //   },
+      // ),
+      // body: ListView(
+      //   // scrollDirection: Axis.horizontal,
+      //   children: [
+      //     Container(
+      //       width: 400,
+      //       height: 400,
+      //       color: Colors.green,
+      //     ),
+      //     Container(
+      //       width: 300,
+      //       height: 300,
+      //       color: Colors.blue,
+      //     ),
+      //     Container(
+      //       width: 200,
+      //       height: 200,
+      //       color: Colors.red,
+      //     ),
+      //     Container(
+      //       width: 100,
+      //       height: 100,
+      //       color: Colors.amber,
+      //     ),
     );
   }
 }
